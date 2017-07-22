e92c620aefb557ad1beaffdc5233275409364823
Jaromil <jaromil@dyne.org>
2017-07-22 08:22:00 +0200

    stable release and travis checks
    verified working with current freecoin codebase, tagging relase as
    succesfully extruding the core library out of the liberator code.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.dyne/freecoin-lib "0.2.0-SNAPSHOT"
+(defproject org.clojars.dyne/freecoin-lib "0.2.0"
   :description "Freecoin digital currency toolkit"
   :url "https://freecoin.dyne.org"
 
