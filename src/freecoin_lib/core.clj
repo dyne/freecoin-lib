88c520423ded7df301c9fa7dc713224ff5c39b0a
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-09-14 16:52:32 +0200

    Renamed the Record to a more generic

--- src/freecoin_lib/core.clj
+++ src/freecoin_lib/core.clj
@@ -309,7 +309,7 @@ Used to identify the class type."
                              :accounts-atom accounts-atom
                              :tags-atom tags-atom})))
 
-(s/defrecord Faircoin2 [rpc-config :- RPCconfig]
+(s/defrecord BtcRpc [rpc-config :- RPCconfig]
   Blockchain
   (list-accounts [bk]
     (btc/listaccounts :config rpc-config))
@@ -318,11 +318,11 @@ Used to identify the class type."
     (btc/getaddressesbyaccount :config rpc-config
                                :account account-id)))
 
-(s/defn ^:always-validate new-faircoin2
+(s/defn ^:always-validate new-btc-rpc
   ([]
    (-> (config/create-config)
        (config/rpc-config)
-       (new-faircoin2)))
+       (new-btc-rpc)))
   ([rpc-config-path :- s/Str]
    (let [rpc-config (btc-conf/read-local-config rpc-config-path)]
-        (s/validate Faircoin2 (map->Faircoin2 {:rpc-config (dissoc rpc-config :txindex :daemon)})))))
+        (s/validate BtcRpc (map->BtcRpc {:rpc-config (dissoc rpc-config :txindex :daemon)})))))
