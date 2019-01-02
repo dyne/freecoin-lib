43d95ff6a33a15606d77eddd5d8f9b6ebeaa361b
Aspasia Beneti <aspasia.beneti@gmail.com>
2019-01-02 14:10:59 +0100

    Got rid of snapshot version

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [clojure-humanize "0.2.2"]
 
                  ;; storage
-                 [org.clojars.dyne/clj-storage "0.9.0-SNAPSHOT"]
+                 [org.clojars.dyne/clj-storage "0.9.0"]
 
                  ;; fxc secret sharing protocol
                  [org.clojars.dyne/fxc "0.5.0"]
